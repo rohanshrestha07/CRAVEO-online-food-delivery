3f1e8c14f2da132581c6b51531bea98010160823
Rohan Shrestha <rohanxtha2060@gmail.com>
2025-01-15 22:35:20 +0545

    logo updated

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -24,7 +24,8 @@ export default function NavBar() {
                 <TopBanner />
                 <div className="container mx-auto h-auto w-full flex justify-between py-2 items-center px-4 sm:px-8 md:px-8 lg:px-0">
                     <div className='mr-32'>
-                        <h1 className="font-extrabold text-[40px] text-[red] cursor-pointer">FOODIE</h1>
+                        {/* <h1 className="font-extrabold text-[40px] text-[red] cursor-pointer">FOODIE</h1> */}
+                        <img className='h-16 w-full' src="public/yum.png" alt="" />
                     </div>
                     {/* Desktop Menu */}
                     <div className='menuList lg:flex hidden items-center'>
